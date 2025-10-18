5b96090bc27400a3922cbea6d50e5c7378e5f29e
NolbertoChagala <21393137@utcancun.edu.mx>
2025-10-17 19:04:02 -0500

    add instal

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
-// src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { registerSW } from "virtual:pwa-register";
+
+
+registerSW({
+  onNeedRefresh() {
+    console.log("🔄 Nueva versión disponible. Recarga para actualizar.");
+  },
+  onOfflineReady() {
+    console.log("✅ App lista para usar sin conexión (offline).");
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate", // Se actualiza solo cuando subes nueva versión
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "PWA-TODO-JEPH-NCC",
+        short_name: "PWA-JEPH-NCC",
+        description: "Una aplicación de lista de tareas simple y efectiva.",
+        start_url: ".",
+        scope: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0078D7",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        // Archivos que se guardarán para modo offline
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
 });
